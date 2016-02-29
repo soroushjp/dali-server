fede2fe396d3925b5ecef2f85d318e8b6d846b2f
Soroush Pour <me@soroushjp.com>
2016-02-28 22:37:40 -0800

    add db migrations

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ Server for Project Dali
 
 ## Build
 
+### Go
+
 - Install [gvm](https://github.com/moovweb/gvm)
 - `gvm install go1.6 && gvm use go1.6`
 - `go get -u github.com/soroushjp/dali-server`
+
+### PostgreSQL
+
+- Install and run PostgreSQL: http://www.postgresql.org/download/
+- `go get bitbucket.org/liamstask/goose/cmd/goose`
+- `goose up`
---
+++ db/migrations/20160228180332_CreateItemsTable.sql
@@ -0,0 +1,14 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  created_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  name VARCHAR (100) NOT NULL,
+  description TEXT NOT NULL,
+  source VARCHAR (100) NOT NULL,
+  url_image VARCHAR (2083) NOT NULL
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
